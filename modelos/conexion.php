68d1adbbff7baf543182c0f050309c6cc3ef2002
Carlos Morales <carlosmoralesl772@gmail.com>
2020-08-16 22:28:35 -0500

    Update conexion.php
    other conf

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,10 +4,10 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=wiad5ra41q8129zn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;
-				dbname=lj7v2vuodml90qtz",
-			            "fyu6pbsuu56adgfo",
-			            "vzr50kreocfaahmj");
+		$link = new PDO("mysql:host=us-cdbr-east-02.cleardb.com;
+				dbname=heroku_202e23678af4aa5",
+			            "b4e9244ef09c92",
+			            "c1f39122");
 
 		$link->exec("set names utf8");
 
