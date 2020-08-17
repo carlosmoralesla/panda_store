5b81a27beb891e59ad1fdeca676d1ba527c5cd69
Carlos Morales <carlosmoralesl772@gmail.com>
2020-08-16 19:12:52 -0500

    Updating connection into Heroku
    Updated information

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,10 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=sis_inventario",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=wiad5ra41q8129zn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;
+				dbname=lj7v2vuodml90qtz",
+			            "fyu6pbsuu56adgfo",
+			            "vzr50kreocfaahmj");
 
 		$link->exec("set names utf8");
 
