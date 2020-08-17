4d44895889e54f3667f523c2344c780a10e0dee6
Carlos Morales <carlosmoralesl772@gmail.com>
2020-08-16 22:32:49 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,8 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=us-cdbr-east-02.cleardb.com;
-				dbname=heroku_202e23678af4aa5",
+		$link = new PDO("mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_202e23678af4aa5",
 			            "b4e9244ef09c92",
 			            "c1f39122");
 
