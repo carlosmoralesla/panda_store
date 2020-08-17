01b6a1be965b674edbb2612c5df9b8d1178880cf
Carlos Morales <carlosmoralesl772@gmail.com>
2020-08-17 00:19:20 -0500

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,15 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-<<<<<<< HEAD
-		$link = new PDO("mysql:host=localhost;dbname=sis_inventario",
-			            "root",
-			            "");
-=======
 		$link = new PDO("mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_202e23678af4aa5",
 			            "b4e9244ef09c92",
 			            "c1f39122");
->>>>>>> 0e7f529b60b0bc3ee632305ee171f7c0713235b8
 
 		$link->exec("set names utf8");
 
@@ -20,8 +14,4 @@ class Conexion{
 
 	}
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0e7f529b60b0bc3ee632305ee171f7c0713235b8
